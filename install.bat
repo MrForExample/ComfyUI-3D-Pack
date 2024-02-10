11ad9d6e968987c3f12818ae73def10496128b61
MrForExample <62230687+MrForExample@users.noreply.github.com>
2024-02-10 10:14:27 +0100

    Update Install

--- install.bat
+++ install.bat
@@ -12,6 +12,13 @@ if exist "%python_miniconda_exec%" (
 
     "%python_miniconda_exec%" -s -m pip install -U xformers --index-url https://download.pytorch.org/whl/cu121
 
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/ashawkey/diff-gaussian-rasterization
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/NVlabs/nvdiffrast/
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/facebookresearch/pytorch3d.git@stable
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/rusty1s/pytorch_scatter.git
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/YixunLiang/simple-knn.git
+    "%python_miniconda_exec%" -s -m pip install -U https://github.com/ashawkey/kiuikit.git
+
     set "python_miniconda_exec=..\..\..\..\..\..\..\python_miniconda_env\ComfyUI\python.exe"
 
     cd tgs/models/snowflake/pointnet2_ops_lib && "%python_miniconda_exec%" -s setup.py install && cd ../../../../
