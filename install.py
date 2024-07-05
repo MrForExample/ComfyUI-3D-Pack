94bb30f3a77ec9793389c6b7e2bd23eb5a9df2b4
Mr. For Example <62230687+MrForExample@users.noreply.github.com>
2024-07-05 14:46:03 -0600

    Add pytorch3d cuda118 wheels for linux

---
+++ install.py
@@ -0,0 +1,5 @@
+# Called after requirements.txt is installed: 
+# https://github.com/ltdrdata/ComfyUI-Manager/blob/386af67a4c34db3525aa89af47a6f78c819926f2/prestartup_script.py#L495
+# https://github.com/ltdrdata/ComfyUI-Manager/blob/386af67a4c34db3525aa89af47a6f78c819926f2/cm-cli.py#L106
+
+
